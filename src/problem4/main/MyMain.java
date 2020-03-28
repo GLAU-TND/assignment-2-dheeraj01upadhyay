30e3c353eba1210dde683ce4164ea67bc8d93214
Karikeya25Pandey <kartikeya.pandey_cs18@gla.ac.in>
2020-03-28 19:47:18 +0530

    main class is made

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,11 +1,24 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:08 PM
- */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj= new MyBinarySearchTree();
+        obj.insert(15);
+        obj.insert(5);
+        obj.insert(7);
+        obj.insert(11);
+        obj.insert(12);
+        obj.insert(8);
+        obj.insert(17);
+        MyQueue obj1 = new MyQueue();
+        obj1.preOrderSucc(obj.root);
+        System.out.print("Pre-Order Successor of all the nodes are: ");
+        obj1.dispPreOrderSucc();
+    }
+
 }
