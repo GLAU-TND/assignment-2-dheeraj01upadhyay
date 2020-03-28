2bfc5829ae30222aba4f833876114414a7c66527
Karikeya25Pandey <kartikeya.pandey_cs18@gla.ac.in>
2020-03-28 19:27:05 +0530

    main class is made is made

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,11 +1,19 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String args[]) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Yash",60);
+        obj.insert("Mohit",39);
+        obj.insert("Muskan",10);
+        obj.insert("Aishwarya",8);
+        obj.insert("Uddeshya",20);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
